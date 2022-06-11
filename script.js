a3f4a2875ff55971d8ceacf306af0af2e00c95f0
Tanishka Talwar <tanishkatalwar18@gmail.com>
2022-06-11 14:16:33 +0530

    use form to get input from user

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const button = document.querySelector('button');
-const input = document.querySelector('input');
+
 const template = document.querySelector('template');
 const list =  document.querySelector('.list');
+
+const title = document.querySelector('input[name="title"]');
+const description = document.querySelector('input[name="description"]');
+const dueDate = document.querySelector('input[name="duedate"]');
+const priority = document.querySelector('select[name="priority"]');
+
+let temp = [title, description, dueDate, priority]
 button.addEventListener('click',function(){
-    let todo = input.value;
     let item = template.content.cloneNode(true);
-    item.querySelector('div').textContent = todo;
+    let paras = item.querySelectorAll('div p');
+    for (let i = 0; i < paras.length; i++) {
+        paras[i].textContent += temp[i].value;
+        temp[i].value = '';
+    }
     list.appendChild(item);
 });
